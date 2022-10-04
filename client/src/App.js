dfd14ad3cddde959c4d558fde2a3a05f9a704e87
bandily <81126679+bandily@users.noreply.github.com>
2022-10-03 21:42:23 -0700

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import HomePage from './Components/HomePage/HomePage';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    fetch('/me', {
+      credentials: 'include'
+    })
+      .then(res => {
+        if(res.ok) {
+          res.json().then(user => {
+            setCurrentUser(user)
+          })
+        }
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='bg'></div>
+    <div className='bg bg2'></div>
+    <div className='bg bg3'></div>
+      <Router>
+        <Fragment>
+          {/* <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser}/> */}
+          <Routes>
+            <Route exact path="/" element={<HomePage currentUser={currentUser}/>}/>
+          </Routes>
+        </Fragment>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/Components/HomePage/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import '../../Styling/HomePage.css'
+
+function HomePage( {currentUser} ) {
+    const history = useNavigate();
+
+    return (
+        <div id="container">
+            <h1 className="first-phase">
+                <span 
+                    className="txt-rotate" 
+                    data-period="2000" 
+                    data-rotate='[ "Welcome to Uplift."]'>
+                </span>
+            </h1>
+            <button 
+                onClick={() => {
+                    currentUser ? history("/profile") : history("/login")
+                }} 
+                className="button-30">
+                <span className="text">Enter</span>
+            </button>
+        </div>
+    )
+}
+
+export default HomePage;
