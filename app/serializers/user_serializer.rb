6e5ac40685484f9e09976020beb406db1764a788
bandily <81126679+bandily@users.noreply.github.com>
2022-10-03 20:37:27 -0700

    serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :age, :weight, :gender, :goal_weight
+  attributes :id, :username, :password, :age, :weight, :gender, :goal_weight
+
+  has_one :avatar
+  has_many :logs
 end
